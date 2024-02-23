2fcc13db0082e4c2a6ebf5f6e15bff387557a2cf
AleksandrAtr <atraleks@gmail.com>
2024-02-23 17:09:08 +0000

    fix: Database link

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tx2^c@xnc)vd^7jv$tigg%%naw^5*w=^9fhsjew=%(kb=p0%p@'
+# SECRET_KEY = 'django-insecure-tx2^c@xnc)vd^7jv$tigg%%naw^5*w=^9fhsjew=%(kb=p0%p@'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -86,7 +87,7 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # }
 
 DATABASES = {
-   'default': dj_database_url.parse('postgres://DATABASE_URL')
+   'default': dj_database_url.parse('DATABASE_URL')
 }
 
 
