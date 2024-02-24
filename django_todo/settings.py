69101c052a5952f4767be2dfb9ca20533433178b
AleksandrAtr <atraleks@gmail.com>
2024-02-24 13:12:02 +0000

    fix: change db argument to actual variable not string

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 DATABASE_URL = os.environ.get('DATABASE_URL')
 
 DATABASES = {
-   'default': dj_database_url.parse('DATABASE_URL')
+   'default': dj_database_url.parse(DATABASE_URL)
 }
 
 
